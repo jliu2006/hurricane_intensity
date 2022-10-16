05e4d0c48ce70e4f637cc30e7bacbd5d229d2f7f
fun <fun@fun.com>
2022-10-16 15:15:14 -0400

    add modis download scripts

---
+++ download_modis.py
@@ -0,0 +1,27 @@
+from modis_tools.auth import ModisSession
+from modis_tools.resources import CollectionApi, GranuleApi
+from modis_tools.granule_handler import GranuleHandler
+
+username = "lxg0601"  # Update this line
+password = "Xian2021"  # Update this line
+
+# Authenticate a session
+session = ModisSession(username=username, password=password)
+
+# Query the MODIS catalog for collections
+collection_client = CollectionApi(session=session)
+
+
+#collections = collection_client.query(short_name="MOD09CMG", version="006")
+collections = collection_client.query(short_name="MODIS_A-JPL-L2P", version="2019.0")
+
+
+# Query the selected collection for granules
+granule_client = GranuleApi.from_collection(collections[0], session=session)
+
+# Filter the selected granules via spatial and temporal parameters
+nigeria_bbox = [-76, 14, -60, 20]
+nigeria_granules = granule_client.query(start_date="2012-10-24", end_date="2012-10-25", bounding_box=nigeria_bbox)
+
+# Download the granules
+GranuleHandler.download_from_granules(nigeria_granules, session)
